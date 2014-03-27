faa90950f23093bda0ec7da55a232cbdb854a40a
kasandell <ksandell14@gmail.com>
2014-03-27 12:33:29 -0700

    Update program.js

--- program.js
+++ program.js
@@ -5,6 +5,7 @@ setInterval(function() {
   for(i=Game.ObjectsById.length-1; i >= 0; i--){
     Game.ObjectsById[i].buy();
   }
+  Game.Objects['Factory'].unlockSpecial();
   Game.ClickCookie();
   Game.Earn(9999999999999999999999999999999999999999999999999999999999999999999999);
 }, 100);
